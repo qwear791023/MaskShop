512eb69985a62b98619ecdd9d8e07eaefe797f0e
qwear791023 <59466396+qwear791023@users.noreply.github.com>
2023-01-09 16:16:44 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -16,7 +16,6 @@ btn_leftBar.addEventListener('click', ()=> {
 
 
 
-
 // Get Date
 let day = document.querySelector('.get-day');
 let date = document.querySelector('.get-date');
@@ -26,27 +25,31 @@ let get_day = new Date();
 // 星期
 let today_num = get_day.getDay();
 day.textContent = dayNumToZh(today_num);
-if ( today_num == 1 ) maskShop.innerHTML = '身分證末號為<h3>1,3,5,7,9</h3>可購買';
-if ( today_num == 2 ) maskShop.innerHTML = '身分證末號為<h3>0,2,4,6,8</h3>可購買';
-if ( today_num == 3 ) maskShop.innerHTML = '身分證末號為<h3>1,3,5,7,9</h3>可購買';
-if ( today_num == 4 ) maskShop.innerHTML = '身分證末號為<h3>0,2,4,6,8</h3>可購買';
-if ( today_num == 5 ) maskShop.innerHTML = '身分證末號為<h3>1,3,5,7,9</h3>可購買';
-if ( today_num == 6 ) maskShop.innerHTML = '身分證末號為<h3>0,2,4,6,8</h3>可購買';
+if ( today_num != 0 ) {
+    if ( today_num%2 == 0 ) {
+        maskShop.innerHTML = '身分證末號為<h3>0,2,4,6,8</h3>可購買';
+    } else if ( today_num%2 == 1 ) {
+        maskShop.innerHTML = '身分證末號為<h3>1,3,5,7,9</h3>可購買';
+    }
+} else { maskShop.innerHTML=''; }
 
 function dayNumToZh(dayNum) {
-    if ( dayNum == 0 ) return '星期日';
-    if ( dayNum == 1 ) return '星期一';
-    if ( dayNum == 2 ) return '星期二';
-    if ( dayNum == 3 ) return '星期三';
-    if ( dayNum == 4 ) return '星期四';
-    if ( dayNum == 5 ) return '星期五';
-    if ( dayNum == 6 ) return '星期六';
+    switch( dayNum ) {
+        case 0: return '星期日';
+        case 1: return '星期一';
+        case 2: return '星期二';
+        case 3: return '星期三';
+        case 4: return '星期四';
+        case 5: return '星期五';
+        case 6: return '星期六';
+    }
 }
+
+
 // 年月日
 let newDate;
 let newMooth;
 if ( get_day.getDate() < 10 ) {
-    // let newDate = get_day.getDate();
     newDate = Math.abs(get_day.getDate()).toString(10);
     newDate = `0${newDate}`;
 } else { newDate = get_day.getDate(); }
@@ -99,136 +102,158 @@ maskXHR.onload = function() {
     console.log(data.length);
 
     let FilterMaskData = [];
-    let OpenTime;
+
 
     // DOM
     const search = document.querySelector('.search');
     const barBody = document.querySelector('.bar-body');
     const btnSearch = document.getElementById('btnSearch');
-
-    // OpenTime
     
 
     function init() {
-        showMarker('all');
+        FilterMask('all');
+        showMarker();
     }
 
     function opentimeCheck(available) {
-        // Get Hours 
         let nowNoon;
-        let Time;
-        let nextDay;
-        let nextNoon;
-        let nextTime;
+        let openTime;
+        let getDay = new Date();
+        let day = getDay.getDay();
+        let nowHour = getDay.getHours();
 
+        if ( 9 <= nowHour && nowHour < 12 ) {
+            nowNoon = '上午看診'
+        } else if ( 14<= nowHour && nowHour < 17 ) {
+            nowNoon = '下午看診'
+        } else if ( 18<= nowHour && nowHour < 21 ) {
+            nowNoon = '晚上看診'
+        } else { nowNoon = undefined }
 
+        openTime = dayNumToZh(day) + nowNoon;
 
-        if ( 9 <= nowHour < 12 ) {
-            nowNoon = '上午看診';
-        } else if ( 14 <= nowHour < 17 ) {
-            nowNoon = '下午看診';
-        } else if ( 18 <= nowHour < 21 ) {
-            nowNoon = '晚上看診';
-        }
-        Time = day + nowNoon;
-
-        function NoonToHours(noon) {
-            if ( noon === '上午看診' ) {
-                return '09:00 ~ 12:00';
-            } else if ( noon = '下午看診' ) {
-                return '14:00 ~ 17:00';
-            } else if ( noon = '晚上看診' ) {
-                return '18:00 ~ 21:00';
-            }
-        }
-
-        if ( available.includes(Time) ) {
-            // 有營業
-            return day + ' ' + NoonToHours(nowNoon);
+        if ( available.includes(openTime) ) {
+            return '營業中 ' + dayNumToZh(day) + NoonToHours(nowNoon)
         } else {
-            // 無營業
-            toNext(today_num, nowHour);
-        }
-
-        // toNext
-        function toNext(day, nowHour) {
-
-            // 21~ & ~9 & 12~14 & 17~18
-            if ( !Time ) {
-                if ( nowHour >= 21 ) { 
-                    nextTime = tomorrow_zh + '上午看診';
-                } else if ( nowHour < 9 ) {
-                    nextTime = day + '上午看診';
-                } else if ( 12 <= nowHour < 14 ) {
-                    nextTime = day + '下午看診';
-                } else if ( nowHour === 17 ) {
-                    nextTime = day + '晚上看診';
+            if( !nowNoon ) {
+                if ( nowHour < 9 ) {
+                    nowNoon = '上午看診'
+                } else if ( 12<= nowHour && nowHour < 14 ) {
+                    nowNoon = '下午看診'
+                } else if ( 17 == nowHour ) {
+                    nowNoon = '晚上看診'
+                } else if ( nowHour >= 21 ) {
+                    nowNoon = '上午看診'
+                    if ( day == 6 ) {
+                        day = 0
+                    } else { day += 1 }
                 }
             }
-
-            while( !available.includes(nextTime) ) {
-                if ( day == 6 ) {
-                    nextDay = 0;
-                } else { nextDay = day + 1 }
-
-                if ( nowNoon === '晚上看診' ) {
-                    nextNoon = '上午看診';
-                    nextTime = dayNumToZh(nextDay) + nextNoon;
-                    day = nextDay;
-                } else {
-                    if ( nowNoon === '上午看診' ) nextNoon = '下午看診';
-                    if ( nowNoon === '下午看診' ) { nextNoon = '晚上看診';}
-                    nextTime = dayNumToZh(day) + nextNoon;
+            openTime = dayNumToZh(day) + nowNoon;
+            if ( available.includes(openTime) ) {
+                return '已打烊，開始營業時間: ' + dayNumToZh(day) + NoonToHours(nowNoon);
+            } else {
+                let count = 0;
+                while ( !available.includes(openTime) ) {
+                    if ( count > 5 ) {
+                        return ' 已打烊 '
+                    } else { count += 1 }
+                    if ( nowNoon == '晚上看診' ) {
+                        if ( day == 6 ) {
+                            day = 0
+                        } else { day += 1 }
+                        nowNoon = '上午看診'
+                    } else {
+                        if ( nowNoon === '上午看診' ) nowNoon = '下午看診'
+                        if ( nowNoon === '下午看診' ) nowNoon = '晚上看診'
+                    }
+                    openTime = dayNumToZh(day) + nowNoon;
                 }
-                nowNoon = nextNoon;
+                openTime = dayNumToZh(day) + NoonToHours(nowNoon);
+                return '已打烊，開始營業時間: ' + openTime;
             }
-
-            if( available.includes(nextTime) ) return '已打烊，開始營業時間為：' + NoonToHours(nowNoon);
         }
     }
 
-    function showMarker(area) {
-        let markers = new L.MarkerClusterGroup().addTo(map);
+    function FilterMask(area) {
         if(area === 'all') {
-            FilterMaskData = data;
-            for(let i = 0; i < data.length; i++) {
-                if (data[i].properties.mask_adult == 0 && data[i].properties.mask_child) {
-                    // 無口罩
-                    markers.addLayer(L.marker([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]], {icon: greyMask})
-                        .bindPopup(`<h1>${data[i].properties.name}</h1><p>成人口罩: ${data[i].properties.mask_adult}<br>兒童口罩:${data[i].properties.mask_child}</p>`));
-                } else {
-                    // 有口罩
-                    markers.addLayer(L.marker([data[i].geometry.coordinates[1], data[i].geometry.coordinates[0]], {icon: greenMask})
-                        .bindPopup(`<h1>${data[i].properties.name}</h1><p>成人口罩: ${data[i].properties.mask_adult}<br>兒童口罩:${data[i].properties.mask_child}</p>`));
-                }
-            }
-            map.addLayer(markers);
+            let maskData = data;
+            FilterMaskData = maskData;
         } else {
-            for(let i = 0; i < data.length; i++) {
-                console.log(typeof(data[i].properties.name));
-                if ( data[i].properties.name.includes(area) ) {
-                    FilterMaskData.push(data[i].properties)
-                } else if ( data[i].properties.address.includes(area) ) {
-                    FilterMaskData.push(data[i].properties)
-                } else if ( data[i].properties.cunli.includes(area) ) {
-                    FilterMaskData.push(data[i].properties)
-                }
+            let maskData = data.filter(mask =>  
+                mask.properties.name.includes(area) || mask.properties.address.includes(area) || mask.properties.cunli.includes(area) );
+            FilterMaskData = maskData;
+        }
+        showBar();
+    }
+
+    function showMarker() {
+        let markers = new L.MarkerClusterGroup().addTo(map);
+        for(let i = 0; i < FilterMaskData.length; i++) {
+            if (FilterMaskData[i].properties.mask_adult == 0 && FilterMaskData[i].properties.mask_child) {
+                // 無口罩
+                markers.addLayer(L.marker([FilterMaskData[i].geometry.coordinates[1], FilterMaskData[i].geometry.coordinates[0]], {icon: greyMask})
+                    .bindPopup(`<h1>${FilterMaskData[i].properties.name}</h1><p>成人口罩: ${FilterMaskData[i].properties.mask_adult}<br>兒童口罩:${FilterMaskData[i].properties.mask_child}</p>`));
+            } else {
+                // 有口罩
+                markers.addLayer(L.marker([FilterMaskData[i].geometry.coordinates[1], FilterMaskData[i].geometry.coordinates[0]], {icon: greenMask})
+                    .bindPopup(`<h1>${FilterMaskData[i].properties.name}</h1><p>成人口罩: ${FilterMaskData[i].properties.mask_adult}<br>兒童口罩:${FilterMaskData[i].properties.mask_child}</p>`));
             }
-            console.log(FilterMaskData);
-            // for ( let j = 0; j < FilterMaskData.length; j++ ) { 
-            //     str += `<div class="card"><h4>${FilterMaskData[j].name}</h4><h5><i class="fas fa-location-dot"></i>${FilterMaskData[j].address}</h5><h5><i class="fas fa-solid fa-phone"></i>${FilterMaskData[j].phone}</h5>
-            //     <h5><i class="fas fa-timer"></i>${opentimeCheck(FilterMaskData[j].available)}</h5>
-            //     <div class="mask">div class="maskAdult"><h6>成人口罩</h6> <h4>${FilterMaskData[j].mask_adult}</h4></div><div class="maskChild"><h6>兒童口罩</h6> <h4>${FilterMaskData[j].mask_child}</h4></div></div></div>`
-            // }
+        }
+        map.addLayer(markers);
+    }
+
+    function showBar() {
+        let str = '';
+        for( let i = 0; i < FilterMaskData.length; i++ ) {
+            str += `<div class="card"><h4>${FilterMaskData[i].properties.name}</h4><h5><i class="fas fa-location-dot"></i>${ToCDB(FilterMaskData[i].properties.address)}</h5><h5><i class="fas fa-solid fa-phone"></i>${FilterMaskData[i].properties.phone}</h5><h5 class="timeControl"><i class="fas fa-timer"></i> ${opentimeCheck(FilterMaskData[i].properties.available)} </h5><div class="mask"><div class="maskAdult"><h6>成人口罩</h6> <h4>${FilterMaskData[i].properties.mask_adult}</h4></div><div class="maskChild"><h6>兒童口罩</h6> <h4>${FilterMaskData[i].properties.mask_child}</h4></div></div></div>`
+        }
+        barBody.innerHTML = str;
+        openORclose();
+    }
+
+    function openORclose() {
+        let timeControl = document.querySelectorAll('.timeControl');
+        for ( let i = 0; i < timeControl.length; i++ ) {
+            if ( timeControl[i].innerText.includes('已打烊') ) {
+                console.log(typeof(timeControl[i]));
+                timeControl[i].classList.add('closeTime');
+            }
+        }
+    }
+
+    function NoonToHours(noon) {
+        switch(noon) {
+            case '上午看診': return '09:00~12:00';
+            case '下午看診': return '14:00~17:00';
+            case '晚上看診': return '18:00~21:00';
         }
     }
 
     function filterMarker() {
-        // console.dir(search.value);
-        showMarker(search.value);
+        FilterMask(search.value);
     }
 
     btnSearch.addEventListener('click', filterMarker);
 
     init();
+}
+
+
+// 全形字轉半形字
+function ToCDB(str) { 
+    var tmp = ""; 
+    for(var i=0;i<str.length;i++){ 
+        if (str.charCodeAt(i) == 12288){
+            tmp += String.fromCharCode(str.charCodeAt(i)-12256);
+            continue;
+        }
+        if(str.charCodeAt(i) > 65280 && str.charCodeAt(i) < 65375){ 
+            tmp += String.fromCharCode(str.charCodeAt(i)-65248); 
+        } 
+        else{ 
+            tmp += String.fromCharCode(str.charCodeAt(i)); 
+        } 
+    } 
+    return tmp 
 }
